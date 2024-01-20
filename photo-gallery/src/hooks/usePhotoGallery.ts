8da62741c6ed5b7b688356adf50ed8426ccc21ae
MSBivens <mikebiv95@gmail.com>
2024-01-19 18:03:01 -0800

    saving photos step complete

--- photo-gallery/src/hooks/usePhotoGallery.ts
+++ photo-gallery/src/hooks/usePhotoGallery.ts
@@ -8,14 +8,49 @@ import { Capacitor } from '@capacitor/core';
 
 export function usePhotoGallery() {
     const takePhoto = async () => {
-      const photo = await Camera.getPhoto({
-        resultType: CameraResultType.Uri,
-        source: CameraSource.Camera,
-        quality: 100,
-      });
-    };
-  
-    return {
-      takePhoto,
-    };
+        const photo = await Camera.getPhoto({
+          resultType: CameraResultType.Uri,
+          source: CameraSource.Camera,
+          quality: 100,
+        });
+      
+        const fileName = Date.now() + '.jpeg';
+        const savedFileImage = await savePicture(photo, fileName);
+        const newPhotos = [savedFileImage, ...photos];
+        setPhotos(newPhotos);
+      };
+    export function usePhotoGallery() {
+        const savePicture = async (photo: Photo, fileName: string): Promise<UserPhoto> => {
+          const base64Data = await base64FromPath(photo.webPath!);
+          const savedFile = await Filesystem.writeFile({
+            path: fileName,
+            data: base64Data,
+            directory: Directory.Data,
+          });
+      
+          // Use webPath to display the new image instead of base64 since it's
+          // already loaded into memory
+          return {
+            filepath: fileName,
+            webviewPath: photo.webPath,
+          };
+        };
+      }
+      
+      export async function base64FromPath(path: string): Promise<string> {
+        const response = await fetch(path);
+        const blob = await response.blob();
+        return new Promise((resolve, reject) => {
+          const reader = new FileReader();
+          reader.onerror = reject;
+          reader.onload = () => {
+            if (typeof reader.result === 'string') {
+              resolve(reader.result);
+            } else {
+              reject('method did not return a string');
+            }
+          };
+          reader.readAsDataURL(blob);
+        });
+      }
   }
